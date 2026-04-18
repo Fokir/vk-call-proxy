6881e78c98689e4d1891dc2a8d0d6094bdde3be6
Andrew <sokol.fokir@gmail.com>
2026-04-18 21:50:16 +0400

    chore: bump version to v0.24.7

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.callvpn.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.24.6"
+        versionCode = 3
+        versionName = "0.24.7"
     }
 
     buildTypes {
